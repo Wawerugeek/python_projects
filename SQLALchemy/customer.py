a52c17f888427c36ec8e70dc2ff7350e8245fee0
Wawerugeek <stevemuiruri84@gmail.com>
2023-06-19 22:51:54 +0300

    more practices

---
+++ SQLALchemy/customer.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+from sqlalchemy import create_engine, MetaData, Table, Integer, String, Column, DateTime, ForeignKey, Numeric, CheckConstraint
+from datetime import datetime 
+
+engine = create_engine("mysql+mysqldb://root:@localhost/my_data")
+engine.connect()
+
+metadata = MetaData()
+
+customers = Table('customers', metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('first_name', String(100), nullable=False),
+    Column('last_name', String(100), nullable=False),
+    Column('username', String(50), nullable=False),
+    Column('email', String(200), nullable=False),
+    Column('address', String(200), nullable=False),
+    Column('town', String(50), nullable=False),
+    Column('created_on', DateTime(), default=datetime.now),
+    Column('updated_on', DateTime(), default=datetime.now, onupdate=datetime.now)
+)
+
+
+items = Table('items', metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('name', String(200), nullable=False),
+    Column('cost_price', Numeric(10, 2), nullable=False),
+    Column('selling_price', Numeric(10, 2),  nullable=False),
+    Column('quantity', Integer(), nullable=False),
+    CheckConstraint('quantity > 0', name='quantity_check')
+)
+
+
+orders = Table('orders', metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('customer_id', ForeignKey('customers.id')),
+    Column('date_placed', DateTime(), default=datetime.now),
+    Column('date_shipped', DateTime())
+)
+
+
+order_lines = Table('order_lines', metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('order_id', ForeignKey('orders.id')),
+    Column('item_id', ForeignKey('items.id')),
+    Column('quantity', Integer())
+)
+
+metadata.create_all(engine)
--- SQLALchemy/first.py
+++ SQLALchemy/first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from sqlalchemy import create_engine, MetaData, Table, String, Column, Text, DateTime, Boolean, Integer
+from sqlalchemy import create_engine, MetaData, Table, String, Column, Text, DateTime, Boolean, Integer, ForeignKey
 from datetime import datetime
 
 engine = create_engine("mysql+mysqldb://root:@localhost/my_data")
@@ -16,5 +16,17 @@ blog = Table ('blog', metadata,
               Column("Created_on", DateTime(), default=datetime.now),
               Column("updated_on", DateTime(), default=datetime.now, onupdate=datetime.now)
               )
+user = Table ('users', metadata, 
+              Column("id", Integer(), primary_key=True),
+              Column("user", String(200), nullable=False),
+              )
+
+Post = Table ('posts', metadata,
+              Column("id", Integer(), primary_key=True),
+              Column("post_title", String(200), nullable=False),
+              Column("content", Text(), nullable=False),
+              Column("user_id", Integer(), ForeignKey(user.c.id)),
+              )
+              
 
 metadata.create_all(engine)
